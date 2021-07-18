69b5f3dcd5c054b0cf433067fbcadbb99fdedc41
ManthanParadhi <66413955+ManthanParadhi@users.noreply.github.com>
2021-07-18 18:36:22 +0530

    nothing much

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import "./Css/App.css";
 import Home from "./Component/Home";
 import Contact from "./Component/Contact";
 import { Route, Switch } from "react-router-dom";
+import Navigation from "./Component/Navigation";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/contact">
-        <Contact />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <div className="grid grid-cols-8">
+      <div className="col-span-1">
+        <Navigation />
+      </div>
+      <div className="col-span-7">
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   );
 }
 
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,20 +1,8 @@
 import React from "react";
-import Navigation from "../Component/Navigation";
 import Carousel from "./Carousel";
 import "../Css/home.css";
 
 const Home = () => {
-  return (
-    <>
-      <div className="grid grid-cols-8">
-        <div className="col-span-1">
-          <Navigation />
-        </div>
-        {/* <div className="col-span-7">
-          <Carousel />
-        </div> */}
-      </div>
-    </>
-  );
+  return <>{/* <Carousel /> */}</>;
 };
 export default Home;
